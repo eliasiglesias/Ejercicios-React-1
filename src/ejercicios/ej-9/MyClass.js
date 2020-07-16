201bc44705eb487e8debd8539cfcd46b25bd1c98
eliasiglesias <61991213+eliasiglesias@users.noreply.github.com>
2020-07-16 16:44:03 +0200

    ejercicios

--- src/ejercicios/ej-9/MyClass.js
+++ src/ejercicios/ej-9/MyClass.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
+import { getMyIp } from "./get-ip.js";
 
 class MyClass extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {ipAddress: '...'};
-    }
+	constructor(props) {
+		super(props);
+		this.state = { ipAddress: ".." };
+	}
 
-    render() {
-        return (
-            <h1>Mi ip es {this.state.ipAddress}</h1>
-        );
-    }
+	async componentDidMount() {
+		const result = await getMyIp();
+		this.setState({ ipAddress: result.ip });
+	}
+
+	render() {
+		return <h1>Mi ip es {this.state.ipAddress}</h1>;
+	}
 }
 
 export default MyClass;
